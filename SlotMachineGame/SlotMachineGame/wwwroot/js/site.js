fdf40da3ed10674b1cb30678ceb59be8258381a7
Brian Hooper <brian_hooper@msn.com>
2024-12-30 20:15:35 -0800

    Added slot animation

--- SlotMachineGame/SlotMachineGame/wwwroot/js/site.js
+++ SlotMachineGame/SlotMachineGame/wwwroot/js/site.js
@@ -1,4 +1,33 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function Init(element) {
+	const box = document.getElementById(element);
+	for (var i = 0; i < 50; i++) {
+		const innerBox = document.createElement("div");
+		innerBox.classList.add("innerBox");
+		innerBox.innerText = i;
+		box.appendChild(innerBox);
+	}
+}
 
-// Write your JavaScript code.
+function Run(element, delay) {
+	new Promise(resolve => setTimeout(resolve, delay)).then(() => {
+		const box = document.getElementById(element);
+		box.classList.remove("position");
+		box.classList.remove("endPosition");
+		box.classList.add("animateRoll");
+	});
+	new Promise(resolve => setTimeout(resolve, delay + 5000)).then(() => {
+		const box = document.getElementById(element);
+		box.classList.remove("animateRoll");
+		box.classList.add("endPosition");
+	});
+}
+
+function Start() {
+	Run("tallBox1", 1000);
+	Run("tallBox2", 2000);
+	Run("tallBox3", 3000);
+}
+
+Init("tallBox1");
+Init("tallBox2");
+Init("tallBox3");
